26ee8b6580e61994ce9616ff3e9215f1d6a4a4ce
Azer <imranovazer@gmail.com>
2022-09-25 23:57:46 +0400

    Fixed Layout

---
+++ src/components/ItemsGrid.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ItemsGrid()
+{
+    return(
+        <div className="ItemsGrid">
+
+        </div>
+    ) ;
+}
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Carusel from "./Carusel";
+import ItemsGrid from "./ItemsGrid";
+
 export default function MainContent()
 {
     return(
     
         <div className="MainContent">
             <Carusel/>
+            <ItemsGrid/>
         </div>
     
     ) ;
