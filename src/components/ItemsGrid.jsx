c46d9d7bc68fb06b5d4cf8a894750733145f175d
Azer <imranovazer@gmail.com>
2022-09-26 00:16:24 +0400

    Fixed Layout

--- src/components/ItemsGrid.jsx
+++ src/components/ItemsGrid.jsx
@@ -4,7 +4,26 @@ export default function ItemsGrid()
 {
     return(
         <div className="ItemsGrid">
+            <div className="SortSettings">
+                <ul>
+                    <li>
+                        ALL
+                    </li>
+                     <li>
+                        ACCESORIES
+                    </li>
+                    <li>
+                        ELECTRONICS
+                    </li>
+                     <li>
+                        SHOES
+                    </li>
 
+                </ul>
+            </div>
+            <div className="ProductsGrid">
+                
+            </div>
         </div>
     ) ;
 }
