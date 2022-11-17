81ed827710ea095d16068660b48e2d8e229d8846
Azer <imranovazer@gmail.com>
2022-11-17 12:44:22 +0400

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ export default function Footer()
 {
     return (
         <div className="Footer">
-            
+                <div className="message" >
+                    Not finished yet. Mat crash while adding to chard same object
+                </div>
         </div>
         
     ) ;
